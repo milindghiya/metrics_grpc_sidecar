29b61be4e39bac9ee17ceee64478eab297f72423
milind <milind.g@amagi.com>
2023-04-05 14:37:23 +0530

    fix another bug

--- examples/mux_http_server/server.go
+++ examples/mux_http_server/server.go
@@ -18,9 +18,9 @@ func newMetricsClient() mtc.MetricsClient {
     mc.Connect("localhost:50051",3)
     mc.AddCounterMetric("request_count",[]string{"host_name","request_path","http_method","status_code"}, "This metric tracks the total number of requests made to the server over a period of time. It can help us identify trends and spikes in traffic.")
     mc.AddCounterMetric("request_error_count",[]string{"host_name","request_path","http_method","status_code"}, "This metric tracks the percentage of requests that result in an error response (e.g., 4xx or 5xx status codes). A high error rate can indicate issues with the server or the application.")
-    mc.AddHistogramMetric("http_request_duration_seconds",[]string{"host_name","request_path","http_method","status_code"}, "Time taken to serve HTTP requests.",  []float64{0.1, 0.5, 1, 2.5, 5, 10, 20})
-    mc.AddHistogramMetric("http_request_size_bytes",[]string{"host_name","request_path","http_method","status_code"}, "Size of HTTP requests.",  []float64{100, 200, 500, 1000, 2000, 5000},)
-    mc.AddHistogramMetric("http_response_size_bytes",[]string{"host_name","request_path","http_method","status_code"}, "Size of HTTP responses.",  []float64{100, 200, 500, 1000, 2000, 5000},)
+    mc.AddHistogramMetric("http_request_duration_seconds",[]string{"host_name","request_path","http_method","status_code"}, "Time taken to serve HTTP requests.",  []float64{0.01, 0.1, 0.5, 1, 2.5, 5, 10, 50})
+    mc.AddHistogramMetric("http_request_size_bytes",[]string{"host_name","request_path","http_method","status_code"}, "Size of HTTP requests.",  []float64{1, 100, 500, 1000, 5000, 10000},)
+    mc.AddHistogramMetric("http_response_size_bytes",[]string{"host_name","request_path","http_method","status_code"}, "Size of HTTP responses.",  []float64{1, 100, 500, 1000, 5000, 10000},)
     return mc
 }
 
@@ -31,13 +31,15 @@ func commonMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         respWriter := rw.NewResponseWriter(w)
         next.ServeHTTP(respWriter, r)
+        route := mux.CurrentRoute(r)
+        path, _  := route.GetPathTemplate()
         duration := time.Since(respWriter.StartTime())
-        metricsClient.ObserveHistogram("http_request_size_bytes", map[string]string{"host_name":r.Host,"request_path": r.URL.Path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() }, float64(r.ContentLength))
-        metricsClient.ObserveHistogram("http_response_size_bytes", map[string]string{"host_name":r.Host,"request_path": r.URL.Path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() }, float64(respWriter.Count()))
-        metricsClient.IncrementCounter("request_count", map[string]string{"host_name":r.Host,"request_path": r.URL.Path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() })
-        metricsClient.ObserveHistogram("http_request_duration_seconds", map[string]string{"host_name":r.Host,"request_path": r.URL.Path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() }, float64(duration.Seconds()))
+        metricsClient.ObserveHistogram("http_request_size_bytes", map[string]string{"host_name":r.Host,"request_path": path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() }, float64(r.ContentLength))
+        metricsClient.ObserveHistogram("http_response_size_bytes", map[string]string{"host_name":r.Host,"request_path": path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() }, float64(respWriter.Count()))
+        metricsClient.IncrementCounter("request_count", map[string]string{"host_name":r.Host,"request_path": path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() })
+        metricsClient.ObserveHistogram("http_request_duration_seconds", map[string]string{"host_name":r.Host,"request_path": path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() }, float64(duration.Seconds()))
         if(respWriter.StatusCode() < 200 || respWriter.StatusCode() >= 400) {
-            metricsClient.IncrementCounter("request_error_count", map[string]string{"host_name":r.Host,"request_path": r.URL.Path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() })
+            metricsClient.IncrementCounter("request_error_count", map[string]string{"host_name":r.Host,"request_path": path,"http_method": r.Method, "status_code": respWriter.StatusCodeStr() })
         }   
 	})
 }
